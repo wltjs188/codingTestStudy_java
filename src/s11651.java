f39ad6ea470e5de3ab1c1a0be03fb6274bdf0ce5
DESKTOP-BBQAPPJ\user <wltjs188@gmail.com>
2021-02-03 17:44:49 +0900

    1181

---
+++ src/s11651.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class s11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for(int i=0;i<n;i++) {
+            String[] str = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(str[0]);
+            arr[i][1] = Integer.parseInt(str[1]);
+        }
+        Arrays.sort(arr,new Comparator<int[]>() { //Comparator 인터페이스 재정의
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1]==o2[1])// y좌표의 값이 같다면 x좌표를 기준으로 정렬
+                    return Integer.compare(o1[0], o2[0]);
+                return Integer.compare(o1[1], o2[1]); // 나머지는 y좌표를 기준으로 정렬
+            }
+        });
+        for(int i=0;i<n;i++) {
+            sb.append(arr[i][0]+" "+arr[i][1]+"\n");
+        }
+        System.out.print(sb);
+    }
+}
---
+++ src/s1181.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class s1181 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        ArrayList<String> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            arr[i] = br.readLine();
+        }
+        Arrays.sort(arr, new Comparator<String>() { //정렬
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length()!=o2.length())
+                    return o1.length()-o2.length();
+                else
+                    return o1.compareTo(o2);
+
+            }
+        });
+
+        for(int i=0;i<n;i++){ //중복제거
+            if(!result.contains(arr[i]))
+                result.add(arr[i]);
+        }
+
+        for(int i=0;i<result.size();i++){
+            sb.append(result.get(i)+"\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
